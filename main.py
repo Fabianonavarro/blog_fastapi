916775f538a8209a3312daae2996de9439d2ed17
Fabianonavarro <nav.info.suporte@gmail.com>
2025-11-14 18:01:21 -0300

    Projeto FastAPI + SQLite pronto para subir

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
-from .routers import contas
+from routers import contas
 import os
 
 app = FastAPI(title="API Bancario")
 
-# Rota principal
 @app.get("/")
 def root():
     return {"message": "API Bancario rodando!"}
 
-# Incluir o router das contas
 app.include_router(contas.router, prefix="/contas")
 
 if __name__ == "__main__":
