5a3acd1a9099e88740cc1bbe2335ad39d76b93ef
Fabianonavarro <nav.info.suporte@gmail.com>
2025-11-17 18:08:03 -0300

    Corrige models e cria estrutura do projeto

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from datetime import date
 
 # ---------- Usuário ----------
 class Usuario(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     nome: str = Field(max_length=100)
     cpf: str = Field(max_length=11, unique=True, index=True)
     data_nascimento: date
@@ -12,7 +12,7 @@ class Usuario(SQLModel, table=True):
 
 # ---------- Conta ----------
 class Conta(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     usuario_id: int = Field(foreign_key="usuario.id")
     numero_conta: int = Field(index=True)
     agencia: str = Field(default="0001", max_length=10)
